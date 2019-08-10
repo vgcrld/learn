296c3261ce08bb1840f6a92dcad1d6ee8124d941
Rich Davis <vgcrld@gmail.com>
2019-08-09 23:38:26 -0400

    test

--- caesar/main.go
+++ caesar/main.go
@@ -8,6 +8,7 @@ func main() {
 
 	a := []string{"a", "b", "c", "d", "e"}
 	fmt.Printf("%T\n", a)
+	fmt.Printf("%s\n", hello("rich"))
 
 }
 
@@ -15,4 +16,8 @@ func translateChar(c int) (int, int, int) {
 	var a int = 0
 	var s int = 0
 	return a, c, s
+}
+
+func hello(name string) string {
+	return "hi, " + name
 }

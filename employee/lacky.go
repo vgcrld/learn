cb9b6b08acbddb1f13bc1bf53960e6a7252af351
Rich Davis <vgcrld@gmail.com>
2019-10-01 18:58:51 -0400

    Do some stuff

---
+++ employee/lacky.go
@@ -0,0 +1,9 @@
+package employee
+
+// Lacky - This creates a low level employee/
+type Lacky struct {
+	Name        string
+	Position    string
+	Age         int
+	Description string
+}
--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 
 func main() {
 
+	var emp1 employee.Lacky
+
+	emp1.Name = "Rich"
+	emp1.Position = "Nobody"
+	emp1.Description = "A guy that is cool."
+	emp1.Age = 49
+
 	fmt.Println("hello")
+	fmt.Println(emp1.Name)
 	fmt.Println(employee.Salary(115343.00, .10))
 
 }

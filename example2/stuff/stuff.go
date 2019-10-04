784c6610039f52338910bcea4763d61638b7d397
Rich Davis <vgcrld@gmail.com>
2019-10-04 15:53:05 -0400

    Separated into various groups

--- example2/stuff/stuff.go
+++
@@ -1,9 +0,0 @@
-package stuff
-
-//Stuff This is stuff.
-type Stuff struct {
-	Computer string
-	House    string
-	Bus      string
-	age      int
-}
--- structs/employee/lacky.go
+++ structs/employee/lacky.go
@@ -1,9 +1,15 @@
 package employee
 
+import "fmt"
+
 // Lacky - This creates a low level employee/
 type Lacky struct {
 	Name        string
 	Position    string
 	Age         int
 	Description string
+}
+
+func (e Lacky) Display() {
+	fmt.Printf("Your age, %s, is %X in hex", e.Name, e.Age)
 }

ba0bf2cff20b995828dcbf5d0d0cd474c2c88d9e
Rich Davis <vgcrld@gmail.com>
2019-10-04 16:24:06 -0400

    stuff

--- quickrun/foreverfor.go
+++ quickrun/foreverfor.go
@@ -2,23 +2,23 @@
 
 package main
 
-
 import (
-  "fmt"
+	"fmt"
 )
-
 
 func main() {
 
+	a := 0
 
-  a := 0
+	//Here we can see how to run forever. Just for{}
+	for {
 
-  for{
+		fmt.Printf("%4d ", a)
+		a++
+		if a > 10000 {
+			break
+		}
 
-    fmt.Printf("%4d ",a)
-    //a++
-    //if a > 10000 { break }
-
-  }
+	}
 
 }
